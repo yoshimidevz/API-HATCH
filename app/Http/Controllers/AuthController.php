71987ee6736570d2f60e59b8d5b57d7b23b65454
yoshimidevz <yoshimidev1@gmail.com>
2025-12-08 10:23:19 -0300

    fix: missclick line

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    use Illuminate\Support\Facades\Hash;
-
     public function login(Request $request)
     {
         $request->validate([
